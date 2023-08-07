525fcfdc480ab143fc53b304d6952c85e18f1f92
Orkun Oksar <orkunoksar@hotmail.com>
2023-08-07 21:22:49 +0300

    script eklendi

--- myscript.js
+++ myscript.js
@@ -2,7 +2,7 @@ const myRule = () => {
   w3.toggleShow('div.myTable')
 }
 const myFunct = () => {
-  w3.addClass('div[class^="dv"] > p ','addMeCls')
+  w3.toggleClass('div[class^="dv"] > p ','addMeCls')
 }
 myShow = w3.slideshow('div .kopecik',0);
 w3.includeHTML();
