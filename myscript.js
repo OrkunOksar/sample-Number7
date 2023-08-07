a56136289bb1b808aae3023810a79cb625f1f343
Orkun Oksar <orkunoksar@hotmail.com>
2023-08-07 15:50:40 +0300

    w3.js yeni özellikler eklendi

--- myscript.js
+++ myscript.js
@@ -1,10 +1,6 @@
-function myFunction(id) {
-    var x = document.getElementById(id);
-    if (x.className.indexOf("w3-show") == -1) {
-      x.className += " w3-show";
-    } else { 
-      x.className = x.className.replace(" w3-show", "");
-    }
-  }
-
-
+const myRule = () => {
+  w3.toggleShow('div.myTable')
+}
+const myFunct = () => {
+  w3.addClass('div[class^="dv"] > p ','addMeCls')
+}
